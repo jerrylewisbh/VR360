99421ec981e1cb14550f32b0ac4f134338b078a3
Jerry <jerrylewisbh@gmail.com>
2020-06-22 17:12:42 -0300

    initial projet

---
+++ Assets/Script/HandAnimator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandAnimator : MonoBehaviour
+{
+    private Animator animator;
+    public bool IsLeftHand = false;
+    private static readonly int Flex = Animator.StringToHash("Flex");
+    private string gripAxis;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        gripAxis = IsLeftHand
+            ? "RightTrigger"
+            : "LeftTrigger";
+    }
+
+    private void Update()
+    {
+        float flexValue = 0;
+
+        flexValue = Input.GetAxis(gripAxis);
+        animator.SetFloat(Flex, flexValue);
+    }
+}
